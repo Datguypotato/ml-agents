dcae34ad9fb6033bc008574e51d206eababe528a
Datguypotato <g.cheung@sintlucasedu.nl>
2019-06-17 09:42:13 +0200

    created environment

---
+++ UnitySDK/Assets/OneVersusOne/Shooter.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using UnityEngine.UI;
+//using UnityEngine.AI;
+using UnityEngine;
+
+public class Shooter : MonoBehaviour 
+{
+
+    public GameObject bulletPrefab;
+    public GameObject bulletShot;
+    public GameObject agentObject;
+
+    public ShooterAgent agent;
+
+	// Use this for initialization
+	void Start () 
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        transform.LookAt(agentObject.transform);
+
+
+		if(agent.IsDone() == true)
+        {
+
+        }
+	}
+}
---
+++ UnitySDK/Assets/OneVersusOne/ShooterAgent.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+//using UnityEngine.UI;
+//using UnityEngine.AI;
+using MLAgents;
+using UnityEngine;
+
+public class ShooterAgent : Agent
+{
+
+    //public Transform floorOffset;
+    public Transform bulletTransform;
+
+    public override void AgentReset()
+    {
+        
+    }
+
+    private void Update()
+    {
+        if(bulletTransform == null)
+        {
+            
+        }
+    }
+}
